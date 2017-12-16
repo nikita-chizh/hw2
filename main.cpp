150a60c3f760f5c2cb8c82cf642c3ca5fb7f947d
nikita-chizh <nikita1993chizh@gmail.com>
2017-12-16 17:34:27 +0300

    f

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char* argv[]){
     auto logger = spdlog::stdout_logger_mt("console");
     logger -> info("version {}", version());
-    std::cout<<"hello world";
+    std::cout<<"hello world1";
     return 0;
 }
