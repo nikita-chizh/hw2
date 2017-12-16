1863e62a6e888911556f2417263a6d3244035731
nikita-chizh <nikita1993chizh@gmail.com>
2017-12-16 21:31:16 +0300

    max words feature

--- hw.h
+++ hw.h
@@ -5,7 +5,7 @@
 #include <sstream>
 int version();
 
-std::vector<std::string> split(const std::string &str, char sign, unsigned int how_many);
+std::vector<std::string> split(const std::string &str, char sign, unsigned int words);
 
 
 
--- test.cpp
+++ test.cpp
@@ -6,17 +6,10 @@
 
 using in_out = std::pair<std::string, std::vector<std::string>>;
 
-BOOST_AUTO_TEST_SUITE(test_suite)
+BOOST_AUTO_TEST_SUITE(test_split)
 
-    BOOST_AUTO_TEST_CASE(test_version) {
-        auto console = spdlog::stdout_logger_st("console");
-        console->info("test version1()");
-
-        BOOST_CHECK(version() > 0);
-    }
-
-    BOOST_AUTO_TEST_CASE(test_split) {
-        std::vector<in_out> tests = {
+    BOOST_AUTO_TEST_CASE(test_split_general) {
+        std::vector<in_out> cases = {
                 {"",     {""}},
                 {"11",    {"11"}},
                 {"..",    {"",   "", ""}},
@@ -24,12 +17,23 @@ BOOST_AUTO_TEST_SUITE(test_suite)
                 {".11",   {"",   "11"}},
                 {"11.22", {"11", "22"}},
         };
-        BOOST_CHECK(split(tests[0].first, '.', 3) == tests[0].second);
-        BOOST_CHECK(split(tests[1].first, '.', 3) == tests[1].second);
-        BOOST_CHECK(split(tests[2].first, '.', 3) == tests[2].second);
-        BOOST_CHECK(split(tests[3].first, '.', 3) == tests[3].second);
-        BOOST_CHECK(split(tests[4].first, '.', 3) == tests[4].second);
-        BOOST_CHECK(split(tests[5].first, '.', 3) == tests[5].second);
+        BOOST_CHECK(split(cases[0].first, '.', 3) == cases[0].second);
+        BOOST_CHECK(split(cases[1].first, '.', 3) == cases[1].second);
+        BOOST_CHECK(split(cases[2].first, '.', 3) == cases[2].second);
+        BOOST_CHECK(split(cases[3].first, '.', 3) == cases[3].second);
+        BOOST_CHECK(split(cases[4].first, '.', 3) == cases[4].second);
+        BOOST_CHECK(split(cases[5].first, '.', 3) == cases[5].second);
+    }
+
+    BOOST_AUTO_TEST_CASE(test_split_with_num) {
+        std::vector<in_out> cases = {
+                {"11.22. sdbvsd", {"11"}},
+                {".11.22. sdbvsd", {""}},
+                {"123.11.22. sdbvsd", {"123"}}
+        };
+        BOOST_CHECK(split(cases[0].first, '.', 1) == cases[0].second);
+        BOOST_CHECK(split(cases[1].first, '.', 1) == cases[1].second);
+        BOOST_CHECK(split(cases[2].first, '.', 1) == cases[2].second);
     }
 
 
