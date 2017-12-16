21e6ef5eca8491470fe98f30a73c23b3d13743f1
nikita-chizh <nikita1993chizh@gmail.com>
2017-12-16 23:16:49 +0300

    without version

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "hw2.h"
 #include <spdlog/spdlog.h>
 int main(int argc, char* argv[]){
-    auto logger = spdlog::stdout_logger_mt("console");
-    logger -> info("hello world");
+
     return 0;
 }
